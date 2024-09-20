c9978470882d6b8f381ade28e015665f5e990beb
gioele-bernardini <gioele.g@outlook.com>
2024-09-20 20:14:04 +0200

    Update: blockchain constructor method implemented.

--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
-	"fmt"
-
-	"rsc.io/quote"
+	"bytes"
+	"crypto/sha256"
 )
 
-func main() {
-	fmt.Println(quote.Hello())
+type BlockChain struct {
+	blocks []*Block
+}
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+
+	return block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+
+	chain.blocks = append(chain.blocks, new)
 }
